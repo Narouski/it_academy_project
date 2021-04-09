e4dd086bfc11dde12cb270db2bb2154b94434bf7
Narouski <Y.Narouki@gmail.com>
2021-04-09 21:47:54 +0300

    changes in print BMI

--- bmi.py
+++ bmi.py
@@ -1,6 +1,6 @@
-height = float(input("Введите ваш рост: "))
+height = float(input("Введите ваш рост : "))
 weight = float(input("Введите ваш вес: "))
 
 BMI = weight / (height/100) ** 2
 
-print(f'Ваш BMI {BMI}')
+print(f'Ваш BMI {round(BMI, 2)}')
